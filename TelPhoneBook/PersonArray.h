a8014ebce532ca8a73de9155378deb4938d46835
Siwon Han <sw51love@naver.com>
2021-11-15 10:52:38 +0900

    3. 전화번호 한사람을 위한 정보 파일(Person.cpp), 전화본호 여러 사람을 저장하기 위함 배열 파일(PersonArray.cpp)등을 작성하고 부가적인 파일 기능 업데이트

---
+++ TelPhoneBook/PersonArray.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Person.h"
+typedef struct _perArray
+{
+    Person arr[1000];
+    int perCount;
+} PersonArray;
+
+void InitPersonArray(PersonArray* pa);
+void UninitPersonArray(PersonArray* pa);
+void AddPersonArray(PersonArray* pa);
+void PrintPersonArray(PersonArray* pa);
--- TelPhoneBook/Util.cpp
+++ TelPhoneBook/Util.cpp
@@ -2,6 +2,7 @@
 
 void Menu()
 {
+	puts("===================");
 	puts("1. 전화번호 등록");
 	puts("2. 전화번호 출력");
 	puts("3. 전화번호 검색");
@@ -9,4 +10,10 @@ void Menu()
 	puts("5. 전화번호 Text 파일 출력");
 	puts("0. 프로그램 종료");
 	puts("");
+}
+
+void Flush()
+{
+	while (getchar() != '\n')
+		;
 }
